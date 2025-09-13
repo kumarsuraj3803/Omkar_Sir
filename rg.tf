5106d9181bf11361c25a1faf3d9addaa49e05863
kumarsuraj3803 <kumarsuraj3803@gmail.com>
2025-09-13 17:47:31 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -12,14 +12,14 @@ provider "azurerm" {
   subscription_id = "8e31d1d4-7aa9-4b0a-9687-fdca97e1bd8c"
   features {}
 }
-resource "azurerm_resource_group" "rg11" {
-  name     = "rgsuraj"
+resource "azurerm_resource_group" "rg101" {
+  name     = "rgsuraj44"
   location = "West Europe"
 }
-resource "azurerm_virtual_network" "vnet22" {
-  name                = "vnetsuraj"
-  location            = azurerm_resource_group.rg11.location
-  resource_group_name = azurerm_resource_group.rg11.name
+resource "azurerm_virtual_network" "vnet202" {
+  name                = "vnetsuraj44"
+  location            = azurerm_resource_group.rg101.location
+  resource_group_name = azurerm_resource_group.rg101.name
   address_space       = ["10.0.0.0/16"]
    
   }
